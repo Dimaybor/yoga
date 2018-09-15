6505a37b664b48dc53e88be85a042f55f4c82a3f
dimaybor <dimayboroda@gmail.com>
2018-09-15 04:20:22 +0300

    staff-slider-2

---
+++ dist/js/common.js
@@ -0,0 +1,15 @@
+// $('.staff').slick({
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   arrows: false,
+//   fade: true,
+//   asNavFor: '.staff-nav'
+// });
+// $('.staff-nav').slick({
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   asNavFor: '.staff',
+//   dots: true,
+//   centerMode: true,
+//   focusOnSelect: true
+// });
