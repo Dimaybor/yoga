c454bf125ea2c302c9c371c70a9c924c663b1c64
dimaybor <dimayboroda@gmail.com>
2018-09-15 21:17:32 +0300

    staff-slider, footer

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,15 +1,16 @@
-// $('.staff').slick({
-//   slidesToShow: 1,
-//   slidesToScroll: 1,
-//   arrows: false,
-//   fade: true,
-//   asNavFor: '.staff-nav'
-// });
-// $('.staff-nav').slick({
-//   slidesToShow: 1,
-//   slidesToScroll: 1,
-//   asNavFor: '.staff',
-//   dots: true,
-//   centerMode: true,
-//   focusOnSelect: true
-// });
+$('.staff').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.staff-nav'
+});
+$('.staff-nav').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  asNavFor: '.staff',
+  dots: true,
+  centerMode: true,
+  centerPadding: false,
+  focusOnSelect: true
+});
