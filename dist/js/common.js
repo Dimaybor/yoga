c69d985ba9d1f0dd4591fa2b9d51785dab94db3f
dimaybor <dimayboroda@gmail.com>
2018-09-17 01:24:29 +0300

    fix gallery

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,31 +1,54 @@
-$('.general-slider').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 4000,
-  arrows: false
-});
+$(document).ready(function() {
 
-$('.about-slider').slick();
+  link         = $('.menu-open');
+  openNav     = $('.open-nav')
+  nav_link     = $('.open-nav a')
+  link.click(function() {
+    link.toggleClass('menu-open--active');
+    openNav.toggleClass('open-nav--active');
+  });
+  nav_link.click(function() {
+    link.toggleClass('menu-open--active');
+    openNav.toggleClass('open-nav--active');
+  });
+  
+  $(".main-menu").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 500);
+  });
 
-$('.staff').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  asNavFor: '.staff-nav'
-});
-$('.staff-nav').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  asNavFor: '.staff',
-  dots: true,
-  centerMode: true,
-  centerPadding: false,
-  focusOnSelect: true
-});
+  $('.general-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false
+  });
 
-$(function() {
-  $('.general-select').selectize();
-    placeholder: true
+  $('.about-slider').slick();
+
+  $('.staff').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.staff-nav'
+  });
+  $('.staff-nav').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.staff',
+    dots: true,
+    centerMode: true,
+    centerPadding: false,
+    focusOnSelect: true
+  });
+
+  $(function() {
+    $('.general-select').selectize();
+      placeholder: true
+  });
+  
 });
